b37a2a3fef08bd58a471918cebe9a502900a4212
Sarah Binion <srhbinion@gmail.com>
2015-09-21 01:46:03 -0500

    added grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,17 @@
 module.exports = function(grunt) {
 
+    grunt.registerTask('default', 'Testing out Grunt logging and task callbacks', function() {
+       grunt.log.writeln('This is our first Grunt task!');
+    });
+ 
+    grunt.registerTask('fun', 'This task is for fun only', function() {
+        grunt.log.writeln('This the *fun* Grunt task');
+    });
+ 
+    grunt.registerTask('serious', 'This task is for serious stuff only', function() {
+        grunt.log.writeln('Wipe that smirk off your face; this is serious.');
+    });
+    
     grunt.registerTask( 'default', [ 'clean', 'copy', 'hapi', 'watch'] );
 
     grunt.registerTask( 'build', [ 'clean', 'copy' ] );
